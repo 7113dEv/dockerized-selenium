df00eb994d4e12ac5ab7a9980ffc551a6a8d089a
7113 <gmaio.dev@gmail.com>
2025-08-02 13:00:19 -0700

    Add Chrome testing. Updated selenium chrome webdriver

--- Tests/TestChrome/test_selenium_docker.py
+++ Tests/TestChrome/test_selenium_docker.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import tempfile
 
 print("Test Execution Started")
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 
+# Use a unique temp dir per session, otherwise chromium error is thrown
+temp_profile_dir = tempfile.mkdtemp(prefix="chrome-profile-")
+options.add_argument(f"--user-data-dir={temp_profile_dir}")
+
 driver = webdriver.Remote(
-    command_executor='http://localhost:4444/wd/hub',
+    command_executor='http://selenium-hub:4444/wd/hub',
     options=options
 )
+
 
 driver.maximize_window()
 time.sleep(5)
